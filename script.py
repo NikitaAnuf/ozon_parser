eab0b1b5d32bd61e466dc08f48353e176babc8a9
NikitaAnuf <nikitaanufriev2003@mail.ru>
2026-03-23 19:16:03 +0500

    pass antibot defence

--- script.py
+++ script.py
@@ -1,8 +1,24 @@
 from argparse import ArgumentParser, Namespace
 import re
+import time
+
+# Парсер на основе Chrome
+from undetected_chromedriver import Chrome
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+from fake_useragent import UserAgent
 
 
 BASE_URL = 'https://www.ozon.ru/'
+# Стандартная пауза для имитирования человека
+MIMIC_PAUSE = 1 # sec
+# Стандартная пауза для ожидания появления элемента
+DRIVER_PATIENCE = 10 # sec
+
 
 # Инициализация парсера аргументов командной строки
 def init_arg_parser() -> ArgumentParser:
@@ -37,9 +53,38 @@ def check_arguments(args: Namespace) -> tuple[str | None, str | None, str | None
     return query, article, None
 
 
+def init_web_driver() -> Chrome:
+    ua = UserAgent()
+    # Рандомный user agent. Нужен, чтобы парсер было сложнее вычислить
+    user_agent = ua.random
+
+    options = Options()
+
+    # options.add_argument('--headless')
+    options.add_argument('--disable-gpu') # Отключаю GPU для стабильности работы
+    options.add_argument('--window-size=1920,1080') # Стандартный размер окна, чтобы вёрстка не изменялась
+    options.add_argument(f'user-agent={user_agent}')
+
+    # При закрытии драйвера в логах возвращалась проигнорированная ошибка дескриптора
+    # Параметр enable_cdp_events в драйвере позволяет её предотвратить
+    driver = Chrome(options=options, enable_cdp_events=True)
+
+    return driver
+
+
 # Общая функция для поиска товара
-def find_product(query: str, article: str) -> tuple[dict | str, str | None]:
-    pass
+def find_product(query: str, article: str) -> tuple[dict | str | None, str | None]:
+    try:
+        driver = init_web_driver()
+    except Exception as ex:
+        return None, f'Ошибка в инициализации веб-драйвера - {ex.__str__()}'
+
+    # Открытие главной страницы
+    driver.get(BASE_URL)
+    # Ожидание проверки антибот защиты
+    time.sleep(MIMIC_PAUSE)
+
+    driver.quit()
 
 
 if __name__ == '__main__':
@@ -57,4 +102,4 @@ if __name__ == '__main__':
         print(error)
         exit()
 
-    print(query, article)
+    find_product(query, article)
